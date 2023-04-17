cb8408a2b7a96152b7ffd4da6bdbb260e8d2bd54
Trent Osterman <trentosterman@gmail.com>
2023-04-17 14:30:57 -0500

    added my buttons that will be used as a filter for my recipes. Also added some styling to add a background and added test data to my database.

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,7 +9,10 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <h1>Cosmic Cuisine</h1>
+        <p>Have you ever wanted to have one spot to store all of your favorite recipes in one place so you don't have to remember which app or book you saw that recipe in? Cosmic Cuisine solves that problem by creating a space that will allow you to post your favorite recipes and be able to categorize them based on what type of recipe it is for example; breakfast, entree, desert, etc..
+          Cosmic Cuisine is a convenient way to store all of your favorite recipes to be used again.
+        </p>
       </div>
     </div>
   );
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -60,14 +60,6 @@ function App() {
             <UserPage />
           </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/info"
-          >
-            <InfoPage />
-          </ProtectedRoute>
-
           <Route
             exact
             path="/login"
