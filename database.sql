16caec378d4ab480ab297faeaaddc5de10576969
Trent Osterman <trentosterman@gmail.com>
2023-04-18 15:55:00 -0500

    updated database.sql to match my postico.

--- database.sql
+++ database.sql
@@ -6,7 +6,8 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+	"avatar" VARCHAR (1000) NOT NULL
 );
 
 CREATE TABLE "recipes" (
@@ -17,3 +18,10 @@ CREATE TABLE "recipes" (
 	"category" VARCHAR (100) NOT NULL,
 	"user_id" INT REFERENCES "user"
 );
+
+INSERT INTO recipes ("image", "title", "ingredients", "directions", "category")
+VALUES ('test image', 'test title', 'test ingredients', 'test directions', 'Breakfast'),
+	('test image', 'test title', 'test ingredients', 'test directions', 'Entree'),
+	('test image', 'test title', 'test ingredients', 'test directions', 'Desert'),
+	('test image', 'test title', 'test ingredients', 'test directions', 'Snack'),
+	('test image', 'test title', 'test ingredients', 'test directions', 'Drink');
