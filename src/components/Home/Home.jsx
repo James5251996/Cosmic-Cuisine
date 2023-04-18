a1f3c3ac1c89956465e1f5cc97371c6224cae434
Trent Osterman <trentosterman@gmail.com>
2023-04-18 10:03:05 -0500

    changed styling of my buuttons and created the togle view to show a list view or a card view.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector, useDispatch } from 'react-redux';
-import { pink } from '@mui/material/colors';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { Button } from '@mui/material';
-import { color } from '@mui/system';
+import { Button, Card, CardContent, CardMedia, Typography, CardActionArea } from '@mui/material';
 import { useEffect } from 'react';
-import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   const recipes = useSelector((store) => store.recipeReducer)
   const dispatch = useDispatch();
+  const [toggleView, setToggleView] = useState(true)
 
 
   useEffect(() => {
-    dispatch({type: 'GET_ALL_RECIPES'})
+    dispatch({ type: 'GET_ALL_RECIPES' })
   }, []);
 
   return (
@@ -26,26 +22,48 @@ function UserPage() {
       {/* Here is where the buttons are made to create a dispatch to filter the recipes from the database */}
       <div>
         <img src={user.avatar}></img>
-        <h1 style={{color:'yellow'}}>Cosmic Space</h1>
-        <Button variant='outlined' sx={{backgroundColor: 'darkorange', color: 'white', width: 100}}>Breakfast</Button>
-        <Button variant='outlined' sx={{backgroundColor: 'red', color: 'white', width: 100}}>Entree</Button>
-        <Button variant='outlined' sx={{backgroundColor: 'purple', color: 'white', width: 100}}>Desert</Button>
-        <Button variant='outlined' sx={{backgroundColor: 'green', color: 'white', width: 100}}>Snack</Button>
-        <Button variant='outlined' sx={{backgroundColor: 'blue', color: 'white', width: 100}}>Drink</Button>
-        <Button variant='outlined' sx={{backgroundColor: 'darkpink', color: 'white', width: 100}}>Clear</Button>
+        <h1 style={{ color: 'yellow' }}>Cosmic Space</h1>
+        <Button variant='outlined' sx={{ backgroundColor: '#fe9392', color: 'white', width: 100 }} onClick={() => dispatch({ type: 'GET_BREAKFAST_RECIPES' })}>Breakfast</Button>
+        <Button variant='outlined' sx={{ backgroundColor: '#6e2c99', color: 'white', width: 100 }} onClick={() => dispatch({ type: 'GET_ENTREE_RECIPES' })}>Entree</Button>
+        <Button variant='outlined' sx={{ backgroundColor: '#e771a2', color: 'white', width: 100 }} onClick={() => dispatch({ type: 'GET_DESERT_RECIPES' })}>Desert</Button>
+        <Button variant='outlined' sx={{ backgroundColor: '#aa4985', color: 'white', width: 100 }} onClick={() => dispatch({ type: 'GET_SNACK_RECIPES' })}>Snack</Button>
+        <Button variant='outlined' sx={{ backgroundColor: '#394baf', color: 'white', width: 100 }} onClick={() => dispatch({ type: 'GET_DRINK_RECIPES' })}>Drink</Button>
+        <Button variant='outlined' sx={{ backgroundColor: 'lightblue', color: 'white', width: 100 }} onClick={() => dispatch({ type: 'GET_ALL_RECIPES' })}>Clear</Button>
       </div>
-      <input placeholder='Search'/>
-      {/* Here is where i will map over the recipes in my database */}
+      <div className='inputsearch'>
+        <input placeholder='Search' />
+        {/* Here is where i will map over the recipes in my database */}
+        {/* Here is a button where a user can toggle thier view between list and card view */}
+        <Button variant='outlined' sx={{ width: 175, height: 40, color: 'white', borderColor: 'yellow' }} onClick={() => setToggleView(!toggleView)}>Card/List View</Button>
+      </div>
       <div>
-        {recipes.map(recipe => {
+
+        {toggleView ? recipes.map((recipe) => {
           return (<>
-            <ul key={recipe.id}>
-              <li style={{color: 'white'}}>{recipe.title}, {recipe.category}</li>
+            <ul>
+              <li key={recipe.id} style={{ color: 'white' }}>{recipe.title}, {recipe.category}</li>
             </ul>
-            </> )
-        })}
+          </>)
+        }) :
+         recipes.map((recipe) => {
+          return (
+            <Card sx={{ maxWidth: 250 }}>
+              <CardActionArea>
+                <CardMedia image='https://www.livewellbakeoften.com/wp-content/uploads/2021/11/Cosmic-Brownies-8.jpg' />
+              </CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='div'>
+                  {recipe.title}
+                </Typography>
+                <Typography>
+                  {recipe.category}
+                </Typography>
+              </CardContent>
+            </Card>
+          );
+          })}
       </div>
-      <Button variant='outlined' sx={{backgroundColor: '', color: 'white', width: 330}}>Add a Recipe</Button>
+      <Button variant='outlined' sx={{ backgroundColor: '', color: 'white', width: 330 }}>Add a Recipe</Button>
     </div>
   );
 }
--- src/redux/sagas/recipe.saga.js
+++ src/redux/sagas/recipe.saga.js
@@ -10,9 +10,59 @@ function* getAllRecipes () {
         console.log(`get all recipe error in client`);
     }
 }
+// my saga to get only breakfast recipes on call
+function* getBreakfastRecipes() {
+    try{
+        const breakfastRecipes = yield axios.get('/api/recipes/breakfast');
+        yield put({ type: 'STORE_RECIPES', payload: breakfastRecipes.data})
+    } catch {
+        console.log(`error in my get breakfast recipe saga`);
+    }
+}
+//my saga to only get entree recipes on call
+function* getEntreeRecipes() {
+    try{
+        const entreeRecipes = yield axios.get('/api/recipes/entree');
+        yield put({ type: 'STORE_RECIPES', payload: entreeRecipes.data})
+    } catch {
+        console.log(`error in my get entree recipe saga`);
+    }
+}
+// my saga to get my desert recipes
+function* getDesertRecipes() {
+    try{
+        const desertRecipes = yield axios.get('/api/recipes/desert');
+        yield put({ type: 'STORE_RECIPES', payload: desertRecipes.data})
+    } catch {
+        console.log(`error in my get desert recipe saga`);
+    }
+}
+// my saga to get my snack recipes
+function* getSnackRecipes() {
+    try{
+        const snackRecipes = yield axios.get('/api/recipes/snack');
+        yield put({ type: 'STORE_RECIPES', payload: snackRecipes.data})
+    } catch {
+        console.log(`error in my get snack recipe saga`);
+    }
+}
+// my saga to get my drink recipes
+function* getDrinkRecipes() {
+    try{
+        const drinkRecipes = yield axios.get('/api/recipes/drink');
+        yield put({ type: 'STORE_RECIPES', payload: drinkRecipes.data})
+    } catch {
+        console.log(`error in my get drink recipe saga`);
+    }
+}
 
 function* recipeSaga () {
-    yield takeLatest('GET_ALL_RECIPES', getAllRecipes)
+    yield takeLatest('GET_ALL_RECIPES', getAllRecipes);
+    yield takeLatest('GET_BREAKFAST_RECIPES', getBreakfastRecipes);
+    yield takeLatest('GET_ENTREE_RECIPES', getEntreeRecipes);
+    yield takeLatest('GET_DESERT_RECIPES', getDesertRecipes);
+    yield takeLatest('GET_SNACK_RECIPES', getSnackRecipes);
+    yield takeLatest('GET_DRINK_RECIPES', getDrinkRecipes);
 }
 
 export default recipeSaga;
