6d366b9ad279dbb4f6111cab9b35a6056e6ca791
Trent Osterman <trentosterman@gmail.com>
2023-04-28 12:53:33 -0500

    fixed a spelling error in my nav bar

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,10 +9,14 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <h1 style={{color:'yellow'}}>Cosmic Cuisine</h1>
+        <h1 style={{color:'yellow'}}>About The App:</h1>
         <p style={{color:'white'}}>Have you ever wanted to have one spot to store all of your favorite recipes in one place so you don't have to remember which app or book you saw that recipe in? Cosmic Cuisine solves that problem by creating a space that will allow you to post your favorite recipes and be able to categorize them based on what type of recipe it is for example; breakfast, entree, desert, etc..
           Cosmic Cuisine is a convenient way to store all of your favorite recipes to be used again.
         </p>
+        <h4 style={{color:'white'}}>LinkedIn:</h4>
+        <p>https://www.linkedin.com/in/trent-osterman-8a4365192/</p>
+        <h4 style={{color:'white'}}>GitHub:</h4>
+        <p>https://github.com/James5251996</p>
       </div>
     </div>
   );
